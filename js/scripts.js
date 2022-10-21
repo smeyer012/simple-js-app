1996e028b53562cc8474f85040112659f8bfe580
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-10-21 18:47:11 -0500

    Adds array of currencies as objects to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-let favoriteFood = "tacos";
-document.write(favoriteFood);
+// saves currency types as objects within the currencyList array
+let currencyList = [
+  {name: "United States Dollar", code: "USD", symbol: "\u0024" },
+  {name: "Euro", code: "EUR", symbol: "\u20AC" },
+  {name: "British Pound Sterling", code: "GPB", symbol: "\u00A3" }
+];
