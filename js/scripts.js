4331f51605ae6ecec5e6f1e11122d1a781eab9c2
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-10-22 16:55:06 -0500

    Fixes single/double quotation inconsistencies in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // saves currency types as objects within the currencyList array
 let currencyList = [
-  {name: "United States Dollar", code: "USD", symbol: "\u0024" },
-  {name: "Euro", code: "EUR", symbol: "\u20AC" },
-  {name: "British Pound Sterling", code: "GPB", symbol: "\u00A3" }
+  {name: 'United States Dollar', code: 'USD', symbol: '\u0024' },
+  {name: 'Euro', code: 'EUR', symbol: '\u20AC' },
+  {name: 'British Pound Sterling', code: 'GPB', symbol: '\u00A3' }
 ];
 
 // loops through list of currency and writes name and symbol into a list item
 for (let i=0; i<currencyList.length; i++) {
   document.write('<li>' + currencyList[i].name + ' (' + currencyList[i].symbol + ')');
   // displays text for European currency
-  if (currencyList[i].code === "EUR") {
+  if (currencyList[i].code === 'EUR') {
     document.write(' - How European!');
   }
   document.write('</li>');
