60ad6a534a3f2d88ca1916d6378536798b8b5e60
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-10-23 01:58:57 -0500

    Modifies code producing list items to use forEach() instead of a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,21 @@ let currencyList = [
 ];
 
 // loops through list of currency and writes name and symbol into a list item
-for (let i=0; i<currencyList.length; i++) {
-  document.write('<li>' + currencyList[i].name + ' (' + currencyList[i].symbol + ')');
+// for (let i=0; i<currencyList.length; i++) {
+//   document.write('<li>' + currencyList[i].name + ' (' + currencyList[i].symbol + ')');
+//   // displays text for European currency
+//   if (currencyList[i].code === 'EUR') {
+//     document.write(' - How European!');
+//   }
+//   document.write('</li>');
+// }
+
+// loops through each object in the currency array and writes the currency name and symbol into a list item
+currencyList.forEach(function(currency) {
+  document.write('<li>' + currency.name + ' (' + currency.symbol + ')');
   // displays text for European currency
-  if (currencyList[i].code === 'EUR') {
+  if (currency.code === 'EUR') {
     document.write(' - How European!');
   }
   document.write('</li>');
-}
+});
