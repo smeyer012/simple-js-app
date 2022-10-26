f081a2b994c2ae3eb73295553edcacb3950b2549
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-10-26 12:26:54 -0500

    checks new object keys against the existing array

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,47 @@ let currencyRepository = (function() {
 
   // creates function to add a currency object to the array
   function add(currency) {
-    // checks that new entry is an object
-    if (typeof currency === "object") {
+    // checks that new entry is an object and tests if the keys match with the existing object
+    if (typeof currency === "object" && checkCurrencyKeys(currency, currencyList[0])) {
       // adds new object to currencyList array
       currencyList.push(currency);
     }
+  }
+
+  // checks if new currency object's keys match currencyList
+  function checkCurrencyKeys(currency, currencyList) {
+    //gets existing and new object array entries
+    let newObjKeys = Object.keys(currency);
+    let currObjKeys = Object.keys(currencyList);
+
+    //sets up utilitarian variables
+    let keyMatch = [];
+    let i = 0;
+
+    //first test that the new object has the same number of keys
+    if (newObjKeys.length === currObjKeys.length) {
+      //loops through existing keys
+      currObjKeys.forEach(function(key) {
+        //checks if new object has each key and creates an array of true/false
+        if(newObjKeys.includes(key)) {
+          keyMatch[i] = true;
+        }
+        else {
+          keyMatch[i] = false;
+        }
+        i++;
+      });
+      //checks array for a false match
+      if(!keyMatch.includes(false)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    else{
+      return false;
+    }
+
   }
 
   // creates function to return all objects in array
