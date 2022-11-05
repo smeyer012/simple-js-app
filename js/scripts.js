2d8ad63ab139aaede26302022d2ad69f3fd6dbb7
smeyer012 <115064549+smeyer012@users.noreply.github.com>
2022-11-05 16:21:54 -0500

    retrieves currency data from API

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
 let currencyRepository = (function() {
 
   // sets up array of currency objects
-  let currencyList = [
-    {name: 'United States Dollar', code: 'USD', symbol: '\u0024', rate: 0.9673 },
-    {name: 'Euro', code: 'EUR', symbol: '\u20AC', rate: 1 },
-    {name: 'British Pound Sterling', code: 'GPB', symbol: '\u00A3', rate: 0.6088 }
+  // let currencyList = [
+  //   {name: 'United States Dollar', code: 'USD', symbol: '\u0024', rate: 0.9673 },
+  //   {name: 'Euro', code: 'EUR', symbol: '\u20AC', rate: 1 },
+  //   {name: 'British Pound Sterling', code: 'GPB', symbol: '\u00A3', rate: 0.6088 }
+  // ];
+
+  let currencyList = [];
+  let comparisonArray = [
+    {name: 'USA', rate: 0.1}
   ];
+  let apiUrl = 'https://api.vatcomply.com/rates';
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      let obj = json.rates;
+      for (const property in obj) {
+        let currency = {
+          name: property,
+          rate: obj[property]
+        };
+        add(currency);
+      }
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
 
   // creates function to add a currency object to the array
   function add(currency) {
     // checks that new entry is an object and tests if the keys match with the existing object
-    if (typeof currency === "object" && checkCurrencyKeys(currency, currencyList[0])) {
+    if (typeof currency === "object" && checkCurrencyKeys(currency, comparisonArray[0])) {
       // adds new object to currencyList array
       currencyList.push(currency);
     }
   }
+
+  // sets up HTML element to display the currency list within
+  let currencies = document.querySelector("#currencies");
 
   // adds new object entry to unordered list
   function addListItem(currency) {
@@ -43,10 +69,10 @@ let currencyRepository = (function() {
   }
 
   // checks if new currency object's keys match currencyList
-  function checkCurrencyKeys(currency, currencyList) {
+  function checkCurrencyKeys(currency, comparisonArray) {
     //gets existing and new object array entries
     let newObjKeys = Object.keys(currency);
-    let currObjKeys = Object.keys(currencyList);
+    let currObjKeys = Object.keys(comparisonArray);
 
     /* loops through each key of an existing object and checks that 
     the new object has the key -- will return false at the first 
@@ -93,25 +119,21 @@ let currencyRepository = (function() {
     add: add,
     addListItem: addListItem,
     getAll: getAll,
+    loadList: loadList,
     searchList: searchList
   };
 
 })();
 
-// tests add function
-currencyRepository.add({ name: 'Japanese Yen', code: 'JPY', symbol: '\u00A5', rate: 101.71 }); // will be added
-currencyRepository.add({ name: 'something', color: 'red', size: 10, function: 'task' }); // will not pass checkCurrencyKeys
-console.log(currencyRepository.getAll());
-
 // assigns array of currency objects to allCurrencies variable
 let allCurrencies = currencyRepository.getAll();
 
-// sets up HTML element to display the currency list within
-let currencies = document.querySelector("#currencies");
-
-// loops through all available currencies and adds them to an unordered list
-allCurrencies.forEach(function(currency) {
-  currencyRepository.addListItem(currency);
+currencyRepository.loadList().then(function() {
+  // Now the data is loaded! 
+  // loops through all available currencies and adds them to an unordered list
+  allCurrencies.forEach(function(currency){
+    currencyRepository.addListItem(currency);
+  });
 });
 
 // executes search function on click of searchbutton
